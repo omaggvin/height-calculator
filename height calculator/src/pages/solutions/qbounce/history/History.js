35d44b4f778e8325f39cdfc6fef787b0e88151e4
omaggvin <omaggvin@gmail.com>
2023-09-06 11:35:16 +0200

    _

--- height calculator/src/pages/solutions/qbounce/history/History.js
+++ height calculator/src/pages/solutions/qbounce/history/History.js
@@ -1,29 +1,7 @@
-import Navbar from "../../../../components/Navbar";
 import ResultList from "../../../../components/ResultList";
 
-const exampleResult = [
-    {
-        name: "result1",
-        timeofsubmission: "2021-09-01T00:00:00.000Z",
-        time: 1,
-        height: 1,
-    },
-    {
-        name: "result2",
-        timeofsubmission: "2021-09-02T00:00:00.000Z",
-        time: 2,
-        height: 2,
-    },
-    {
-        name: "result3",
-        timeofsubmission: "2021-09-03T00:00:00.000Z",
-        time: 3,
-        height: 3,
-    },
-];
 export default function History() {
-    const items = JSON.parse(localStorage.getItem('QBounceHistory')) || [];
-
+    const items = JSON.parse(localStorage.getItem("QBounceHistory")) || [];
 
     return (
         <>
